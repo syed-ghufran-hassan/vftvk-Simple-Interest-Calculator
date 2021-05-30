42a88fdc466dd2676bc1dbaf1fd80c760bfd82af
Syed Ghufran Hassan <syedghufranhassan@gmail.com>
2021-05-30 22:24:21 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function compute()
+{  var principal = document.getElementById("principal").value;
+ var rate = document.getElementById("rate").value;
+     var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100
+     var year = new Date().getFullYear()+parseInt(years);
+ function updateRate() 
 {
-    p = document.getElementById("principal").value;
-    
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
 }
         
