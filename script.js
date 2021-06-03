637b2c860a6ea5715ecf60b43b94869239d3d58d
Syed Ghufran Hassan <syedghufranhassan@gmail.com>
2021-06-03 20:36:57 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
-// complete function of calculating the interest
-function compute()
-{
-	var amount = document.getElementById("amount").value;
-	var rate = document.getElementById("rate").value;
-	var years = document.getElementById("years").value;
-	var interest = amount * years * rate /100;	 
-
-	var year = new Date().getFullYear()+parseInt(years);
-
-	var reslt = document.getElementById("result").value;
-
-	if(amount<1 && amount==null){
-		alert("Please enter a positive number!");
-	}
-
-	document.getElementById("result").innerHTML="If you deposit "+amount+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+/* fonction pour récupérer les valeurs aléatoires du rate*/
+function rateValue(val){
+	document.getElementById('rateInput').value=val;
 
 }
 
-function updateRate() 
-{
-    var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+/* fonction de calcul de l'intérêt*/
+function calculate(){
+	/* déclaration des variables*/
+amount = document.getElementById('amount').value;
+rateInput= document.getElementById('rateInput').value;
+selectId=document.getElementById('selectId').value;
+resultat=document.getElementById('resultat');
+
+/* calcul du nombre d'année*/
+var today= new Date();
+var annee = today.getFullYear();
+var result1=((annee=+selectId));
+var today2=new Date();
+var annee2 =today2.getFullYear();
+var result2=result1+annee2
+
+/* calul et affichage de l'intérêt*/
+resultat.innerHTML="If you deposit"+"&nbsp;"+(amount)+","+"<br/>"+ "at an interest rate of"+"&nbsp;"+(rateInput)+"."+"<br/>"+
+"You will receive an amount of"+"&nbsp;"+(amount*rateInput*selectId/100)+","+"<br/>"+
+"in the year"+"&nbsp;"+(result2);
 }
