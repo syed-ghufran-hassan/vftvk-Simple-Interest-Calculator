84589523f9ec94a466b5a02b4e54e430daaf7b66
Syed Ghufran Hassan <syedghufranhassan@gmail.com>
2021-06-03 20:28:08 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,25 @@
-
-function handleSubmit(event)
+// complete function of calculating the interest
+function compute()
 {
-    event.preventDefault();
-    
-    var p = document.getElementById("principal").value;
-    
-    if(p<=0) {
-        
-        document.getElementById("simpleintrest").innerHTML = ": " + " :  " + "Value of Principal Cannot be or zero negative";
-        alert("Value cannot be negative or zero")
-    }
-    else if (p>0){
+	var amount = document.getElementById("amount").value;
+	var rate = document.getElementById("rate").value;
+	var years = document.getElementById("years").value;
+	var interest = amount * years * rate /100;	 
 
-    
-        var r = document.getElementById("rate").value;
-    var t = document.getElementById("time").value;
-    var r1 = r/100;
-    var t1 = t/12;
-    var si = p*r1*t1;
-    document.getElementById("simpleintrest").innerHTML = ": " + " :  " + si;
-    }
-    
+	var year = new Date().getFullYear()+parseInt(years);
+
+	var reslt = document.getElementById("result").value;
+
+	if(amount<1 && amount==null){
+		alert("Please enter a positive number!");
+	}
+
+	document.getElementById("result").innerHTML="If you deposit "+amount+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+
 }
 
-function handleChange(){
-    
-    var r = document.getElementById("rate").value;
-    document.getElementById("change").innerHTML = r+"%";
-
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
